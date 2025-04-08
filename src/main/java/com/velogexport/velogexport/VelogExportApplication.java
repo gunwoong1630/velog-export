d967418000e2af35de83335ce14b1772dd740bba
gunwoong <gwj0421@gmail.com>
2025-04-08 15:17:59 +0900

    init : first commit

---
+++ src/main/java/com/velogexport/velogexport/VelogExportApplication.java
@@ -0,0 +1,13 @@
+package com.velogexport.velogexport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelogExportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VelogExportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/velogexport/velogexport/VelogExportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.velogexport.velogexport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VelogExportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
